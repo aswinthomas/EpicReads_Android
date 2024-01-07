d82714e49c49b04041fa877796ed69528dd52f0b
aswinthomas <aswinthomas@gmail.com>
2024-01-07 20:49:44 +0800

    send password reset

--- app/src/main/java/com/aswindev/epicreads/LoginActivity.kt
+++ app/src/main/java/com/aswindev/epicreads/LoginActivity.kt
@@ -3,9 +3,20 @@ package com.aswindev.epicreads
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.util.Log
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
+import androidx.core.view.setPadding
 import com.aswindev.epicreads.databinding.ActivityLoginBinding
+import com.google.android.material.internal.ViewUtils.dpToPx
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
@@ -21,10 +32,12 @@ class LoginActivity : AppCompatActivity() {
         binding.buttonLogin.setOnClickListener {
             loginUser(binding.editTextEmail.text.toString().trim(), binding.editTextPassword.text.toString().trim())
         }
-        binding.textViewRegister.setOnClickListener {
+        binding.buttonRegister.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+        binding.textViewForgotPassword.setOnClickListener { showResetPasswordDialog() }
     }
+
 
     private fun loginUser(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
@@ -36,7 +49,7 @@ class LoginActivity : AppCompatActivity() {
                 } else {
                     task.exception?.let {
                         Log.d("LoginActivity", "Login failed for User $email: ${it.message}")
-                        val message = it.localizedMessage?:"An unknown error occurred"
+                        val message = it.localizedMessage ?: "An unknown error occurred"
                         AlertDialog.Builder(this)
                             .setTitle("Error Logging User")
                             .setMessage(message)
@@ -45,6 +58,26 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
             }
+    }
 
+    private fun showResetPasswordDialog() {
+        val emailInput = EditText(this)
+        emailInput.inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+        emailInput.hint = "Enter your email"
+
+        AlertDialog.Builder(this)
+            .setTitle("Reset Password")
+            .setView(emailInput)
+            .setPositiveButton("Reset") { _, _ ->
+                auth.sendPasswordResetEmail(emailInput.text.toString().trim())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Snackbar.make(binding.root, "Reset password email sent", Snackbar.LENGTH_LONG).show()
+                        } else {
+                            Snackbar.make(binding.root, "Failed to send reset email", Snackbar.LENGTH_LONG).show()
+                        }
+                    }
+            }
+            .show()
     }
 }
